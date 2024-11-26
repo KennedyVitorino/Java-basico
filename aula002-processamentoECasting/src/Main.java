b34f5ac6199a20a62d664d8e867e6db0550dd93f
Kennedy <elpessimista@gmail.com>
2024-11-26 20:25:03 -0300

    Aula 002 - Processamento de dados e Casting.

---
+++ aula002-processamentoECasting/src/Main.java
@@ -0,0 +1,63 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Exemplo I
+        int x, y;
+
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println();
+
+        // Exemplo II
+        int x1;
+        double y1;
+
+        x1 = 5;
+        y1 = 2 * x1;
+
+        System.out.println(x1);
+        System.out.println(y1);
+        System.out.println();
+
+        // Exemplo III
+        double baseMenor, baseMaior, altura, area;
+
+
+        baseMenor = 6.0;
+        baseMaior = 8.0;
+        altura = 5.0;
+
+        area = (baseMenor + baseMaior) / 2.0 * altura;
+
+        System.out.println(area);
+        System.out.println();
+
+        // Exemplo IV
+        int a, b;
+        double resultado;
+
+        a = 5;
+        b = 2;
+
+        resultado = (double) a / b;
+
+        System.out.println(resultado);
+        System.out.println();
+
+        // Exemplo V
+        double a1;
+        int b1;
+
+        a1 = 5.0;
+        b1 = (int) a1;
+
+        System.out.println(b1);
+
+
+
+
+    }
+}
