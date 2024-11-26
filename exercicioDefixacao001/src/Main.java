ebdd197faf3c7bb445dd0bff5db8043bb7326fbf
Kennedy <elpessimista@gmail.com>
2024-11-25 22:04:20 -0300

    Exercício de fixação - Aula 001

--- exercicioDefixacao001/src/Main.java
+++ exercicioDefixacao001/src/Main.java
@@ -1,19 +1,29 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
 
-        String product1 = "Computer";
-        String product2 = "Office desk";
+        String productOne = "Computer";
+        String productTwo = "Office desk";
 
         int age = 33;
         int code = 5290;
         char gender = 'F';
 
-        double price1 = 1324.0;
-        double price2 =  654.5;
+        double priceOne = 1324.0;
+        double priceTwo =  654.5;
         double measure = 53.12343;
 
-        System.out.println("Products: %n");
-        System.out.printf("");
+        System.out.println("Products:");
+        System.out.printf("%s, which price is $ %.2f%n", productOne, priceOne);
+        System.out.printf("%s, which price is $ %.2f%n", productTwo, priceTwo);
+        System.out.println();
+        System.out.printf("Record %d years old, code %d and gender %c%n", age, code, gender);
+        System.out.println();
+        System.out.printf("Measure with eight decimal places: %.8f%n", measure);
+        System.out.printf("Rounded (three decimal places): %.3f%n", measure);
 
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.3f", measure);
     }
 }
