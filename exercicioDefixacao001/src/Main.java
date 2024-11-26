eca43a29adb927777a199fc73a0b84ae47c72ba6
Kennedy <elpessimista@gmail.com>
2024-11-25 21:35:13 -0300

    Exercício de fixação - Aula 001

---
+++ exercicioDefixacao001/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        int age = 33;
+        int code = 5290;
+        char gender = 'F';
+
+        double price1 = 1324.0;
+        double price2 =  654.5;
+        double measure = 53.12343;
+
+        System.out.println("Products: %n");
+        System.out.printf("");
+
+    }
+}
